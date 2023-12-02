e39d290890958d35baac3f52c5aa1b149cd8cb80
Sahar Saoudi <151698641+sahar-saoudi@users.noreply.github.com>
2023-12-02 08:54:50 -0500

    Update bnRandom.h

--- Jeu_combat_naval/ConsoleApplication2/bnRandom.h
+++ Jeu_combat_naval/ConsoleApplication2/bnRandom.h
@@ -1,19 +1,11 @@
-
-// fichier : bnRandom.h
-// auteur : Vincent Ducharme
-// modifié par : Marie-Flavie Auclair-Fortier - 15 octobre
-// modifié par : Marie-Flavie Auclair-Fortier - 16 mars
-// résumé : Ce fichier contient une fonction de génération de valeur aléatoire
-
-
 #ifndef BNRANDOM_H
 #define BNRANDOM_H
 
-// description : génère une valeur aléatoire entière entre deux bornes
+// description : gÃ©nÃ¨re une valeur alÃ©atoire entiÃ¨re entre deux bornes
 // param[E] : entier, valeur minimale que peut prendre la valeur
 // param[E] : entier, valeur max que peut prendre la valeur
 // sorties : aucune
-// retour : entier, valeur aléatoire produite
+// retour : entier, valeur alÃ©atoire produite
 // pre : min < max
 // post : le retour est entre min et max
 
